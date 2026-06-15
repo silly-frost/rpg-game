2cfe6057ea84662dad30d869193f0dd2a5e49336
happy-squirell <frostisfridge@gmail.com>
2026-06-15 23:59:47 +0300

    refactor(map): rewrote the code and adapted it for npc.cpp

--- headers/map.hpp
+++ headers/map.hpp
@@ -2,12 +2,11 @@
 #define MAP_HPP
 
 #include<ctime>
-#include<string>
 #include<thread>
 #include<chrono>
 #include<cstdlib>
 
 void map_time(); // время на карте (привязано к времени пользователя)
-int go_to_location(); // перемещение к выбранной локации
+std::string go_to_location(); // перемещение к выбранной локации
 
 #endif
--- src/map.cpp
+++ src/map.cpp
@@ -5,10 +5,9 @@
 #include<iostream>
 #include<string>
 
-std::string home = "Дом"; // (id = 0)
-std::string desert = "Пустыня"; // (id = 1)
-std::string unnamed_kingdom = "Безымянное королевство"; // (id = 2)
-int void_counter;
+static std::string home = "Дом"; // (id = 0)
+static std::string desert = "Деревня в пустыне"; // (id = 1)
+static std::string unnamed_kingdom = "Безымянное королевство"; // (id = 2)
 
 void map_time(){
     std::time_t now = std::time(nullptr);
@@ -19,62 +18,44 @@ void map_time(){
     char buffer[80];
     std::strftime(buffer, sizeof(buffer), "%H:%M", &local_time);
 
-    std::cout << "Текущее время на локации: " << buffer << std::endl;
+    std::cout << "Текущее время на локации: " << buffer << "\n" << std::endl;
 }
 
-int go_to_location(){
-wrong_loc: //goto метка для того, чтобы начать заново функцию, при ошибке пользователя, а также небольшой фишкой
-    int loc_id;
-    std::string player_choose;
-    
-    if(player_choose.empty() || player_choose == home){
+
+
+
+std::string go_to_location(){
+wrong_loc: //goto метка для того, чтобы начать заново функцию, при ошибке пользователя
+    std::string p_choose;
+
+    if(p_choose.empty() || p_choose == home){
         std::cout << "Список доступных локаций: \n"
-                << "1. Пустыня \n"
-                << "2. Безымянное королевство \n\n"
-                << "Напиши навание локации, в которую хочешь отправиться: ";
-        std::getline(std::cin, player_choose);
+                << "1. " << desert << "\n"
+                << "2. " << unnamed_kingdom << "\n\n"
+                << "Напишите навание локации, в которую хотите отправиться: ";
+        std::getline(std::cin, p_choose);
     }
-    else if(player_choose == desert){
+    else if(p_choose == desert){
         std::cout << "Список доступных локаций: \n"
-                << "1. Дом \n"
-                << "2. Безымянное королевство \n\n"
-                << "Напиши навание локации, в которую хочешь отправиться: ";
-        std::getline(std::cin, player_choose);
+                << "1. " << home << "\n"
+                << "2. " << unnamed_kingdom << "\n\n"
+                << "Напите навание локации, в которую хотите отправиться: ";
+        std::getline(std::cin, p_choose);
     }
-    else if(player_choose == unnamed_kingdom){
+    else if(p_choose == unnamed_kingdom){
         std::cout << "Список доступных локаций: \n"
-                << "1. Дом \n"
-                << "2. Пустыня \n\n"
-                << "Напиши навание локации, в которую хочешь отправиться: ";
-        std::getline(std::cin, player_choose);
+                << "1. " << home << "\n"
+                << "2. " << desert << "\n\n"
+                << "Напишите навание локации, в которую хотите отправиться: ";
+        std::getline(std::cin, p_choose);
     }
 
-    if(void_counter > 2){
-         #ifdef _WIN32
-        std::system("cls");
-    #else
-        std::system("clear");
-    #endif
-
-        std::cout << "Вы оказались в пустоте! Вас окутывает страх, а тело перестаёт слушаться...\n"
-                  << "Текущее время на локации: NULL";
-        std::this_thread::sleep_for(std::chrono::seconds(4));
-        // в будущем у этой локации будет свой npc
-    }
-    
-    if(player_choose != home && player_choose != desert && player_choose != unnamed_kingdom){
-        std::cout << "\nВыбрана неизвестная локация. Выберите заново, либо отправьтесь в указанный путь. \n"
-                  << "Однако будьте осторожны, многие пропали без вести, пройдя по неизвестному маршруту. \n";
-        void_counter++;
+    if(p_choose != home && p_choose != desert && p_choose != unnamed_kingdom){
+        std::cout << "\nВыбрана неизвестная локация. Выберите заново и будьте осторожны. \n\n";
         goto wrong_loc;
     }
-
-    if(player_choose == home) loc_id = 0;
-    else if(player_choose == desert) loc_id = 1;
-    else if(player_choose == unnamed_kingdom) loc_id = 2;
-    else loc_id = -1;
-    
-    std::cout << "\nОтправляемся в " << player_choose << ". \nВремя в пути: 3 секунды \n";
+   
+    std::cout << "\nОтправляемся в локацию " << p_choose << ". \nВремя в пути: 3 секунды \n";
 
     // Остановка на 3 секунды
     std::this_thread::sleep_for(std::chrono::seconds(3));
@@ -88,7 +69,7 @@ wrong_loc: //goto метка для того, чтобы начать занов
     std::cout << "Вы прибыли! ";
     map_time();
 
-    return loc_id;
+    return p_choose;
 }
 
 #endif
