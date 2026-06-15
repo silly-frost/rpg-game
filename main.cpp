5f321a136d9f220f1bde4ee3f149a68446c5b74b
happy-squirell <frostisfridge@gmail.com>
2026-06-16 00:04:37 +0300

    test(npc): added tests for methods from npc.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include"map.hpp"
+#include"npc.hpp"
 
 int main(){
-    go_to_location();
+    std::string p_choose = go_to_location();
+    loc_npc_list(p_choose);
     return 0;
 }
