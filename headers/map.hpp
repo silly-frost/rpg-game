dd0b08ac4167c9a771df6eb41c46c2d37d0d3310
frost <frostisfridge@gmail.com>
2026-06-20 13:31:45 +0300

    feat: completely rewrote the entire map system
    Now map is not chain of "else if", it's more readable. now locations can
    be easy added

--- headers/map.hpp
+++ headers/map.hpp
@@ -4,12 +4,54 @@
 #include<ctime>
 #include<thread>
 #include<chrono>
+#include<unordered_map>
+#include<vector>
 
-#define dialogue_time std::this_thread::sleep_for(std::chrono::seconds(2))
-#define location static std::string
+inline void time_stop(int seconds) {
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+}
 
-void map_time(); // время на карте (привязано к времени пользователя)
-std::string go_to_location(std::string p_choose); // перемещение к выбранной локации
-void map_description(std::string p_choose); // описание локации
+enum class locationID {
+  // Лесная зона
+  home,
+  flower_valley,
+  dark_forest,
+  broken_village,
+  oak_forest, 
+  
+  // Пустынная зона
+  desert,
+  unnamed_kingdom,
+  tavern,
+  oasis,
+  desert_village,
+
+  // Снежная зона
+  snowlands,
+  ice_caves,
+  snow_village,
+  seaport,
+
+  // Проклятые земли
+  iron_island,
+  cursed_castle,
+};
+
+struct Connection {
+    locationID target; 
+    int travel_time;   
+};
+
+struct Location {
+  std::string name;
+  std::string description;
+  std::vector<Connection> connected_locations;
+};
+
+extern const std::unordered_map<locationID, Location> world_map;
+extern locationID current_location;
+
+void map_time();
+void change_location();
 
 #endif
--- src/map.cpp
+++ src/map.cpp
@@ -1,33 +1,139 @@
 #ifndef MAP_CPP
 #define MAP_CPP
 
+#include<string>
 #include"map.hpp"
 #include"utils.hpp"
 #include<iostream>
-#include<string>
 
-// Лесная зона
-location home = "Дом"; 
-location flower_valley = "Долина цветов";
-location old_forest = "Старый лес";
-location broken_village = "Разрушенная деревня";
-
-// Пустынная зона
-location desert = "Пустыня";
-location unnamed_kingdom = "Безымянное королевство";
-location oasis = "Оазис";
-location desert_village = "Деревня в пустыне";
-
-// Снежная зона
-location snowlands = "Заснеженные земли";
-location ice_caves = "Ледяные пещеры";
-
-// Проклятые земли
-location iron_island = "Железный остров";
-location cursed_castle = "Проклятый замок";
-
-// Наказание :)
-location punishment = ":)";
+const std::unordered_map<locationID, Location> world_map = {
+  {locationID::home, {
+      "Дом",
+      "Уютное место, где вам всегда рады.",
+      {
+        {locationID::desert, 3},
+        {locationID::flower_valley, 2}
+      }
+  }},
+  {locationID::desert, {
+      "Пустыня",
+      "Страшная жара и бесконечные пески.",
+      {
+        {locationID::home, 3},
+        {locationID::unnamed_kingdom, 3},
+        {locationID::oasis, 6}
+      }
+  }},
+  {locationID::oasis, {
+      "Оазис",
+      "Помощь Божья для заблудших в бесконечных песках",
+      {
+        {locationID::desert, 6},
+        {locationID::desert_village, 4}
+      }
+  }},
+  {locationID::desert_village, {
+      "Пустынная деревня",
+      "Здесь проживают сломленные духом люди, потерявшие веру в мир.",
+      {
+        {locationID::oasis, 4}
+      }
+  }},
+  {locationID::unnamed_kingdom, {
+      "Безымянное королевство",
+      "Несмотря на расположение в пустыне, король смог создать рай для своих жителей.",
+      {
+        {locationID::desert, 3},
+        {locationID::tavern, 1}
+      }
+  }},
+  {locationID::tavern, {
+      "Таверна",
+      "Дружественная атмосфера и множество выпивки!",
+      {
+        {locationID::unnamed_kingdom, 1}
+      }
+  }},
+  {locationID::flower_valley, {
+      "Цветочная поляна",
+      "Жужжание пчёл и прекрасные виды наполняют вас решимостью.",
+      {
+        {locationID::home, 2},
+        {locationID::oak_forest, 3}
+      }
+  }},
+  {locationID::oak_forest, {
+      "Дубовый лес",
+      "Вокруг вас шумят вековые деревья.",
+      {
+        {locationID::flower_valley, 3},
+        {locationID::dark_forest, 5}
+      }
+  }},
+  {locationID::dark_forest, {
+      "Темный лес",
+      "Старый жуткий лес, в котором легко заблудиться.",
+      {
+        {locationID::oak_forest, 5},
+        {locationID::broken_village, 2},
+        {locationID::snowlands, 6}
+      }
+  }},
+  {locationID::broken_village, {
+      "Разрушенная деревня",
+      "Когда-то здесь было шумно и весело, пока не начался пожар...",
+      {
+        {locationID::dark_forest, 2}
+      }
+  }},
+  {locationID::snowlands, {
+      "Снежные земли",
+      "Холод пробирает вас до костей.",
+      {
+        {locationID::dark_forest, 6},
+        {locationID::ice_caves, 4},
+        {locationID::snow_village, 2}
+      }
+  }},
+  {locationID::snow_village, {
+      "Снежная деревня",
+      "Горящие здесь огни являются путеводной звездой для заблудших в буре.",
+      {
+        {locationID::snowlands, 2},
+        {locationID::seaport, 1}
+      }
+  }},
+  {locationID::ice_caves, {
+      "Ледяные пещеры",
+      "Мрачная, но безопасная пещера. Здесь можно спрятаться от бури.",
+      {
+        {locationID::snowlands, 4}
+      }
+  }},
+  {locationID::seaport, {
+      "Заснеженный порт",
+      "После обнаружения металлического заражения всем запретили покидать материк.",
+      {
+        {locationID::snowlands, 1}, // Исправлено логическое замыкание: порт соединен со snow_village, а snow_village со snowlands. Если порт ведет сразу в snowlands, то время 1 сек.
+        {locationID::iron_island, 10}
+      }
+  }},
+  {locationID::iron_island, {
+      "Железный остров",
+      "Некогда прекрасное место, но однажды всё изменилось...",
+      {
+        {locationID::seaport, 10},
+        {locationID::cursed_castle, 2}
+      }
+  }},
+  {locationID::cursed_castle, {
+      "Проклятый замок",
+      "Здесь началось распространение железного вируса... Никто не выжил...",
+      {
+        {locationID::iron_island, 2}
+      }
+  }}
+};
 
 void map_time(){
     std::time_t now = std::time(nullptr);
@@ -42,170 +148,55 @@ void map_time(){
 }
 
 
-std::string go_to_location(std::string p_choose){
-    clear_screen();
+locationID current_location = locationID::home;
 
-    std::cout << "Вам будет предоставлен список локаций, в которые можно отправиться. \n";
-    dialogue_time;
-    std::cout << "В некоторые локации нельзя попасть просто так. Вы должны методом проб и ошибок самостоятельно искать путь. \n";
-    dialogue_time;
-    std::cout << "Однако, если вы попробуете обмануть систему, то понесёте наказание. Вы предупреждены. \n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(4));
-
-    clear_screen();
-
-    if(p_choose.empty() || p_choose == home){
-        std::cout << "Список доступных локаций: \n"
-                << "1. " << desert << "\n"
-                << "2. " << flower_valley << "\n" 
-                << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-
-        if(p_choose != desert && p_choose != flower_valley) p_choose = punishment;
-    }
-    else if(p_choose == desert){
-         std::cout << "Список доступных локаций: \n"
-                << "1. " << home << "\n"
-                << "2. " << unnamed_kingdom << "\n"
-                << "3. " << oasis << "\n"
-                << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-
-        if(p_choose != home && p_choose != unnamed_kingdom && p_choose != oasis) p_choose = punishment;
-    }
-    else if(p_choose == oasis){
-        std::cout << "Список доступных локаций: \n"
-            << "1. " << desert << "\n"
-            << "2. " << desert_village << "\n" 
-            << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-
-        if(p_choose != desert && p_choose != desert_village) p_choose = punishment;
-    }
-    else if(p_choose == flower_valley){
-         std::cout << "Список доступных локаций: \n"
-                << "1. " << home << "\n"
-                << "2. " << old_forest << "\n"
-                << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-
-        if(p_choose != home && p_choose != old_forest) p_choose = punishment;
-    }
-    else if(p_choose == old_forest){
-        std::cout << "Список доступных локаций: \n"
-                << "1. " << flower_valley << "\n"
-                << "2. " << broken_village << "\n" 
-                << "3. " << snowlands << "\n"
-                << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-
-        if(p_choose != flower_valley && p_choose != broken_village && p_choose != snowlands) p_choose = punishment;
-    }
-    else if(p_choose == snowlands){
-         std::cout << "Список доступных локаций: \n"
-                << "1. " << old_forest << "\n"
-                << "2. " << ice_caves << "\n"
-                << "3. " << iron_island << "\n" 
-                << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-
-        if(p_choose != old_forest && p_choose != ice_caves && p_choose != iron_island) p_choose = punishment;
-    }
-    else if(p_choose == iron_island){
-         std::cout << "Список доступных локаций: \n"
-                << "1. " << snowlands << "\n"
-                << "2. " << cursed_castle << "\n"
-                << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-
-        if(p_choose != snowlands && p_choose != cursed_castle) p_choose = punishment;
-    }
-    else if(p_choose == cursed_castle){
-        std::cout << "Список доступных локаций: \n"
-                << "1. " << iron_island << "\n"
-                << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-
-        if(p_choose != iron_island) p_choose = punishment;
-    }
-    else if(p_choose == desert_village){
-        std::cout << "Список доступных локаций: \n"
-                << "1. " << oasis << "\n"
-                << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-
-        if(p_choose != oasis) p_choose = punishment;
-    }
-    else if(p_choose == unnamed_kingdom){
-        std::cout << "Список доступных локаций: \n"
-                << "1. " << desert << "\n"
-                << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-
-        if(p_choose != desert) p_choose = punishment;
-    }
-    else if(p_choose == ice_caves){
-        std::cout << "Список доступных локаций: \n"
-                << "1. " << snowlands << "\n"
-                << "Напишите навание локации, в которую хотите отправиться: ";
-        std::getline(std::cin, p_choose);
-        
-        if(p_choose != snowlands) p_choose = punishment;
-    }
-    else {
-        std::cout << "Вы находитесь в неизвестной локации. Перемещу вас домой через 4 секунды.\n";
-        p_choose = home;
-        std::this_thread::sleep_for(std::chrono::seconds(4));
-    }
-
-    clear_screen();
-
-    if(p_choose == punishment){
-        std::cout << "Добро пожаловать в пустоту! Вы здесь не просто так. \n";
-        dialogue_time; 
-        std::cout << "В самом начале я вас предупреждал, что нарушение правил ведёт к наказанию.\n";
-        dialogue_time;
-        std::cout << "Вашим наказанием будет путь домой. \n";
-        dialogue_time;
-        std::cout << "Но идти вам придётся немного дольше, чем обычно...\n";
-        dialogue_time;
-
-        clear_screen();
-        
-        std::cout << "Время в пути: 5 минут :)\n";
-
-        std::this_thread::sleep_for(std::chrono::seconds(300));
-        std::cout << "Вы прибыли, впредь больше не нарушайте правила. \n";
-        return home;
-    }
-
-    std::cout << "Отправляемся в локацию \"" << p_choose << "\"!\n";
-
-    int time_in_travel = 0;
-    if (p_choose == "flower_valley" || p_choose == "oasis" || 
-        p_choose == "broken_village" || p_choose == "cursed_castle") {
-        time_in_travel = 3;
-    } else if (p_choose == "desert" || p_choose == "desert_village" || 
-               p_choose == "old_forest" || p_choose == "iron_island" || 
-               p_choose == "snowlands") {
-        time_in_travel = 5;
-    } else if (p_choose == "unnamed_kingdom" || p_choose == "ice_caves" || 
-               p_choose == "home") {
-        time_in_travel = 4;
-    }
-
-    std::cout << "Время в пути: " << travelTime << " секунд \n";
-    std::this_thread::sleep_for(std::chrono::seconds(travelTime));
-    std::cout << "Вы прибыли! \n";
-}
-
-    clear_screen();
+void change_location(){
+  clear_screen();
+  const Location& current = world_map.at(current_location);
     
-    std::cout << "Вы прибыли! ";
-    map_time();
+    std::cout << "\nВы находитесь в локации \"" << current.name << "\".\n";
+    std::cout << current.description << "\n\n";
+    std::cout << "Доступные пути:\n";
+    
+    for (const Connection& conn : current.connected_locations) {
+        std::cout << "- " << world_map.at(conn.target).name << "\n";
+    }
+    
+    std::cout << "\nКуда вы хотите пойти? ";
+    std::string choice;
+    std::getline(std::cin, choice);
+    
+    bool moved = false;
+    for (const Connection& conn : current.connected_locations) {
+        const Location& next_loc = world_map.at(conn.target);
+        
+        if (next_loc.name == choice) {
+            clear_screen();
+            std::cout << "Вы отправляетесь в локацию \"" << next_loc.name << "\".\n";
+            std::cout << "Путь займет " << conn.travel_time << " сек.\n\nВ пути";
+            std::cout << std::flush;
+            
+            for (int i = 0; i < conn.travel_time; ++i) {
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+                std::cout << " ." << std::flush;
+            }
 
-    return p_choose;
+            clear_screen();
+            std::cout << "\nВы прибыли!\n";
+            map_time();
+
+            current_location = conn.target;
+            moved = true;
+            break;
+        }
+    }
+  
+  if(!moved){ 
+      clear_screen();
+      std::cout << "Название указано неверно." << "\n";
+      time_stop(3);
+      clear_screen();
+  }
 }
 
 #endif
