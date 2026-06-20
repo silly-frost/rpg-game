976cd148b85a043c76b6943efa99fe409f02644c
frost <frostisfridge@gmail.com>
2026-06-20 23:38:31 +0300

    feat: mooved struct Monster and enter checker

--- headers/utils.hpp
+++ headers/utils.hpp
@@ -1,13 +1,33 @@
+
 #ifndef UTILS_HPP
 #define UTILS_HPP
+
+#include <iostream>
+#include <string>
 #include <cstdlib>
 
-inline void clear_screen() {
-    #ifdef _WIN32
-        std::system("cls");
-    #else
-        std::system("clear");
-    #endif
+struct Monster {
+    std::string name;
+    int health;
+    int damage;
+    int gold_reward;
+};
+
+inline void wait_for_player() {
+    std::cout << "\nНажмите Enter, чтобы продолжить...";
+    std::string pause;
+    std::getline(std::cin, pause);
 }
 
+inline void clear_screen() {
+#ifdef _WIN32
+    std::system("cls");
+#else
+    std::system("clear");
 #endif
+}
+
+bool start_battle(Monster enemy);
+
+#endif // UTILS_HPP
+
